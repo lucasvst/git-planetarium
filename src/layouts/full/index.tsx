51cc4d369d0223422da614223761b2df850435ce
Lucas Teles <lucas.teles@zflow.com.br>
2025-10-02 16:52:33 -0300

    feat: adds alias resolver to vite

--- src/layouts/full/index.tsx
+++ src/layouts/full/index.tsx
@@ -1,7 +1,8 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import classNames from '../../utils/classNames'
 import { Outlet } from 'react-router'
+
+import classNames from '@/utils/classNames'
 
 const user = {
   name: 'Tom Cook',
